021b189a4b8dd1de98e17c80ba306aa96d211a3b
Robert Plugge <robmplugge@hotmail.com>
2015-08-11 13:06:55 -0500

    Added comments form/view/controller logic

--- app.js
+++ app.js
@@ -9,13 +9,13 @@ function($stateProvider, $urlRouterProvider) {
       url: '/home',
       templateUrl: '/home.html',
       controller: 'MainCtrl'
-    });
+    })
     
     .state('posts', {
       url: '/posts/{id}',
       templateUrl: '/posts.html',
       controller: 'PostsCtrl'
-    })
+    });
     
   $urlRouterProvider.otherwise('home');
 }])
@@ -37,7 +37,11 @@ function($stateProvider, $urlRouterProvider) {
       $scope.posts.push({
         title: $scope.title, 
         link: $scope.link,
-        upvotes: 0
+        upvotes: 0,
+        comments: [
+          {author: 'Joe', body: 'Cool post!', upvotes: 0},
+          {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
+        ]
       });
       $scope.title = '';
       $scope.link = '';
@@ -46,12 +50,22 @@ function($stateProvider, $urlRouterProvider) {
       post.upvotes += 1;
     };
   }
-]);
+])
 
 .controller('PostsCtrl', [
   '$scope',
   '$stateParams',
-  'posts',
+  'posts', 
   function($scope, $stateParams, posts){
+    $scope.post = posts.posts[$stateParams.id];
     
+    $scope.addComment = function(){
+      if($scope.body === '') { return;}
+      $scope.post.comments.push({
+        body: $scope.body,
+        author: 'user',
+        upvotes: 0
+      });
+      $scope.body = '';
+    }
 }]);
