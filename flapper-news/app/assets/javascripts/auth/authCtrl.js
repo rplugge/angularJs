6a141194f3e209346ff25278aa77c295233d7ced
Robert Plugge <robmplugge@hotmail.com>
2015-08-13 11:18:19 -0500

    Adding authCtrl

---
+++ flapper-news/app/assets/javascripts/auth/authCtrl.js
@@ -0,0 +1,18 @@
+angular.module('flapperNews')
+.controller('AuthCtrl', [
+'$scope',
+'$state',
+'Auth',
+function($scope, $state, Auth){
+}]);
+
+
+angular.module('flapperNews')
+.controller('AuthCtrl', [
+  '$scope',
+  '$state',
+  'Auth',
+  function($scope, $state, Auth){
+    
+  }
+])
